2ae913de080685680e0da7fe61180048c87e19ca
Alvin <alvinmartindwrk@gmail.com>
2023-06-05 11:52:26 +0700

    initial commit
    with spring web, spring data jpa, validation, lombok, and mysql driver

---
+++ src/main/java/com/alvinmdj/springbootrestfulapi/SpringBootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.alvinmdj.springbootrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulApiApplication.class, args);
+	}
+
+}
