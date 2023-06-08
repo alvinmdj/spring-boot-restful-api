2595e3a317166feb8ec01b330687ff850b8ca8a6
Alvin <alvinmartindwrk@gmail.com>
2023-06-08 21:12:25 +0700

    feat: create get user controller

--- src/main/java/com/alvinmdj/springbootrestfulapi/controller/UserController.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.alvinmdj.springbootrestfulapi.controller;
 
+import com.alvinmdj.springbootrestfulapi.entity.User;
 import com.alvinmdj.springbootrestfulapi.model.RegisterUserRequest;
+import com.alvinmdj.springbootrestfulapi.model.UserResponse;
 import com.alvinmdj.springbootrestfulapi.model.WebResponse;
 import com.alvinmdj.springbootrestfulapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,18 @@ public class UserController {
   public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
     userService.register(request);
     return WebResponse.<String>builder().data("OK").build();
+  }
+
+  @GetMapping(
+    path = "/api/users/current",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  // set User entity as parameter to trigger UserArgumentResolver
+  public WebResponse<UserResponse> get(User user) {
+    UserResponse userResponse = userService.get(user);
+
+    return WebResponse.<UserResponse>builder()
+      .data(userResponse)
+      .build();
   }
 }
