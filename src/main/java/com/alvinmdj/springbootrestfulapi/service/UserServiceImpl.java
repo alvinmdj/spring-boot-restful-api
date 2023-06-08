4e33e951d8fdc4954dafddc54eb787fc2944e92c
Alvin <alvinmartindwrk@gmail.com>
2023-06-08 20:02:24 +0700

    refactor: create user service interface & impl

---
+++ src/main/java/com/alvinmdj/springbootrestfulapi/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.alvinmdj.springbootrestfulapi.service;
+
+import com.alvinmdj.springbootrestfulapi.entity.User;
+import com.alvinmdj.springbootrestfulapi.model.RegisterUserRequest;
+import com.alvinmdj.springbootrestfulapi.repository.UserRepository;
+import com.alvinmdj.springbootrestfulapi.security.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserServiceImpl implements UserService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private ValidationService validationService;
+
+  @Transactional
+  public void register(RegisterUserRequest request) {
+    // validate request
+    validationService.validate(request);
+
+    // check username availability
+    if (userRepository.existsById(request.getUsername())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already registered");
+    }
+
+    // create user
+    User user = new User();
+    user.setUsername(request.getUsername());
+    user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+    user.setName(request.getName());
+
+    userRepository.save(user);
+  }
+}
