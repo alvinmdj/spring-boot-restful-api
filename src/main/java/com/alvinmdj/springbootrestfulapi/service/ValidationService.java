6a1aad6c148109149ae78e816a45ad2cc81ed8f4
Alvin <alvinmartindwrk@gmail.com>
2023-06-08 19:59:35 +0700

    refactor: create validation service interface & impl

--- src/main/java/com/alvinmdj/springbootrestfulapi/service/ValidationService.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/service/ValidationService.java
@@ -1,24 +1,5 @@
 package com.alvinmdj.springbootrestfulapi.service;
 
-import com.alvinmdj.springbootrestfulapi.model.RegisterUserRequest;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Validator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Set;
-
-@Service
-public class ValidationService {
-  @Autowired
-  private Validator validator;
-
-  // throw exception if request doesn't conforms with the model validation
-  public void validate(Object request) {
-    Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
-    if (constraintViolations.size() != 0) {
-      throw new ConstraintViolationException(constraintViolations);
-    }
-  }
+public interface ValidationService {
+  void validate(Object request);
 }
---
+++ src/main/java/com/alvinmdj/springbootrestfulapi/service/ValidationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.alvinmdj.springbootrestfulapi.service;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ValidationServiceImpl implements ValidationService {
+  @Autowired
+  private Validator validator;
+
+  // throw exception if request doesn't conforms with the model validation
+  public void validate(Object request) {
+    Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+    if (constraintViolations.size() != 0) {
+      throw new ConstraintViolationException(constraintViolations);
+    }
+  }
+}
