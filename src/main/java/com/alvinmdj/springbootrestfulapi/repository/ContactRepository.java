30d7903311ad2adf6b4462f46dffb9ea7e183373
Alvin <alvinmartindwrk@gmail.com>
2023-06-12 20:32:07 +0700

    feat: add find by user and id method on contact repository

--- src/main/java/com/alvinmdj/springbootrestfulapi/repository/ContactRepository.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/repository/ContactRepository.java
@@ -1,9 +1,13 @@
 package com.alvinmdj.springbootrestfulapi.repository;
 
 import com.alvinmdj.springbootrestfulapi.entity.Contact;
+import com.alvinmdj.springbootrestfulapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
+  Optional<Contact> findFirstByUserAndId(User user, String id);
 }
