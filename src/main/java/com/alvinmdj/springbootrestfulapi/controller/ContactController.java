4cd26965d5378661cc71a3812ca9046e007e9d85
Alvin <alvinmartindwrk@gmail.com>
2023-06-12 20:32:47 +0700

    feat: create get contact controller

--- src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
@@ -7,9 +7,7 @@ import com.alvinmdj.springbootrestfulapi.model.WebResponse;
 import com.alvinmdj.springbootrestfulapi.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ContactController {
@@ -25,6 +23,20 @@ public class ContactController {
   // set User entity as parameter to trigger UserArgumentResolver.
   public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
     ContactResponse contactResponse = contactService.create(user, request);
+
+    return WebResponse.<ContactResponse>builder()
+      .data(contactResponse)
+      .build();
+  }
+
+  @GetMapping(
+    path = "/api/contacts/{contactId}",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  // requires X-API-TOKEN header value so we need to:
+  // set User entity as parameter to trigger UserArgumentResolver.
+  public WebResponse<ContactResponse> get(User user, @PathVariable("contactId") String contactId) {
+    ContactResponse contactResponse = contactService.get(user, contactId);
 
     return WebResponse.<ContactResponse>builder()
       .data(contactResponse)
