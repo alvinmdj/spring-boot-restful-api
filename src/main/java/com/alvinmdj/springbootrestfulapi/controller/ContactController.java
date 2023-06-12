5d709bd1a510696ed13774b2e6a48831f4f25c41
Alvin <alvinmartindwrk@gmail.com>
2023-06-12 21:01:14 +0700

    feat: create update contact controller

--- src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.alvinmdj.springbootrestfulapi.controller;
 import com.alvinmdj.springbootrestfulapi.entity.User;
 import com.alvinmdj.springbootrestfulapi.model.ContactResponse;
 import com.alvinmdj.springbootrestfulapi.model.CreateContactRequest;
+import com.alvinmdj.springbootrestfulapi.model.UpdateContactRequest;
 import com.alvinmdj.springbootrestfulapi.model.WebResponse;
 import com.alvinmdj.springbootrestfulapi.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,27 @@ public class ContactController {
   // set User entity as parameter to trigger UserArgumentResolver.
   public WebResponse<ContactResponse> get(User user, @PathVariable("contactId") String contactId) {
     ContactResponse contactResponse = contactService.get(user, contactId);
+
+    return WebResponse.<ContactResponse>builder()
+      .data(contactResponse)
+      .build();
+  }
+
+  @PutMapping(
+    path = "/api/contacts/{contactId}",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  // requires X-API-TOKEN header value so we need to:
+  // set User entity as parameter to trigger UserArgumentResolver.
+  public WebResponse<ContactResponse> update(
+    User user,
+    @RequestBody UpdateContactRequest request,
+    @PathVariable("contactId") String contactId
+  ) {
+    request.setId(contactId);
+
+    ContactResponse contactResponse = contactService.update(user, request);
 
     return WebResponse.<ContactResponse>builder()
       .data(contactResponse)
