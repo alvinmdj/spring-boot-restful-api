110fc5e053d95b9ac3886fb1b44038bb3a838047
Alvin <alvinmartindwrk@gmail.com>
2023-06-16 21:33:47 +0700

    feat: create search contacts controller

--- src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/controller/ContactController.java
@@ -1,16 +1,16 @@
 package com.alvinmdj.springbootrestfulapi.controller;
 
 import com.alvinmdj.springbootrestfulapi.entity.User;
-import com.alvinmdj.springbootrestfulapi.model.ContactResponse;
-import com.alvinmdj.springbootrestfulapi.model.CreateContactRequest;
-import com.alvinmdj.springbootrestfulapi.model.UpdateContactRequest;
-import com.alvinmdj.springbootrestfulapi.model.WebResponse;
+import com.alvinmdj.springbootrestfulapi.model.*;
 import com.alvinmdj.springbootrestfulapi.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -75,11 +75,47 @@ public class ContactController {
     path = "/api/contacts/{contactId}",
     produces = MediaType.APPLICATION_JSON_VALUE
   )
+  // requires X-API-TOKEN header value so we need to:
+  // set User entity as parameter to trigger UserArgumentResolver.
   public WebResponse<String> delete(User user, @PathVariable("contactId") String contactId) {
     contactService.delete(user, contactId);
 
     return WebResponse.<String>builder()
       .data("OK")
+      .build();
+  }
+
+  @GetMapping(
+    path = "/api/contacts",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  // requires X-API-TOKEN header value so we need to:
+  // set User entity as parameter to trigger UserArgumentResolver.
+  public WebResponse<List<ContactResponse>> search(
+    User user,
+    @RequestParam(value = "name", required = false) String name,
+    @RequestParam(value = "email", required = false) String email,
+    @RequestParam(value = "phone", required = false) String phone,
+    @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+    @RequestParam(value = "size", required = false, defaultValue = "10") Integer size
+  ) {
+    SearchContactRequest request = SearchContactRequest.builder()
+      .page(page)
+      .size(size)
+      .name(name)
+      .email(email)
+      .phone(phone)
+      .build();
+
+    Page<ContactResponse> contactResponses = contactService.search(user, request);
+
+    return WebResponse.<List<ContactResponse>>builder()
+      .data(contactResponses.getContent())
+      .paging(PagingResponse.builder()
+        .currentPage(contactResponses.getNumber())
+        .totalPage(contactResponses.getTotalPages())
+        .size(contactResponses.getSize())
+        .build())
       .build();
   }
 }
