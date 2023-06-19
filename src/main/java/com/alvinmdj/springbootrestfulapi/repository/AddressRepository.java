64511a7a0fdc5f2f0d4e60a3061ee85dd2cb6f12
Alvin <alvinmartindwrk@gmail.com>
2023-06-19 21:33:24 +0700

    feat: add find first by contact and id to address repository

--- src/main/java/com/alvinmdj/springbootrestfulapi/repository/AddressRepository.java
+++ src/main/java/com/alvinmdj/springbootrestfulapi/repository/AddressRepository.java
@@ -1,9 +1,13 @@
 package com.alvinmdj.springbootrestfulapi.repository;
 
 import com.alvinmdj.springbootrestfulapi.entity.Address;
+import com.alvinmdj.springbootrestfulapi.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
+  Optional<Address> findFirstByContactAndId(Contact contact, String id);
 }
